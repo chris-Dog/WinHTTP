1d7e5810775e65dc58a0f44b0072a9c99e8b091e
Peter Molchanov <peter@bitkibble.com>
2014-12-12 14:42:18 -0800

    Removed pause from build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
 call "%VS110COMNTOOLS%\VsDevCmd.bat"
 msbuild WinHTTP.sln /t:Rebuild /p:Configuration=Release /p:Platform="Win32"
 msbuild WinHTTP.sln /t:Build /p:Configuration=Debug /p:Platform="Win32"
-PAUSE
